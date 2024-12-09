aeb8b829d4487e61f017be8a972a0116cff2e5a4
fermartini1 <hola.soymartini1992@gmail.com>
2024-12-09 20:25:27 -0300

    primer comit

---
+++ APIDicsys/DycsysApi/Models/Gasto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DycsysApi.Models
+{
+    public partial class Gasto
+    {
+        public int Id { get; set; }
+        public float Importe { get; set; }
+        public string Dia { get; set; } = null!;
+        public string Mes { get; set; } = null!;
+        public string Anio { get; set; } = null!;
+        public int NombreGastoId { get; set; }
+        public int TipoGastoId { get; set; }
+        public string UsuarioId { get; set; } = null!;
+        public string Detalle { get; set; } = null!;
+        public int? Pagado { get; set; }
+    }
+}
